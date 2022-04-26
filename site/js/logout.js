ee7b26078e4c878850ce35b8ddc339263008f470
aarav18 <aaravshah18@gmail.com>
2022-04-25 23:02:27 -0600

    clear all cookies on logout

--- site/js/logout.js
+++ site/js/logout.js
@@ -2,6 +2,14 @@ function signOut() {
 
   console.log("Signing Out!");
 
-  document.cookie = document.cookie.replace("CrewCentreSession=Valid", "");
+  // document.cookie = document.cookie.replace("CrewCentreSession=Valid", "");
+  var cookies = document.cookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
   window.location.href = "https://aarav18-snhs.netlify.app/login.html";
 }
