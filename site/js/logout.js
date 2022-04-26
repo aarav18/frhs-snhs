29a4b735f2fe19902347c27edb5353016918fbbd
aarav18 <aaravshah18@gmail.com>
2022-04-25 22:52:13 -0600

    try onclick

--- site/js/logout.js
+++ site/js/logout.js
@@ -1,4 +1,7 @@
 function signOut() {
+
+  console.log("Signing Out!");
+
   document.cookie = document.cookie.replace("CrewCentreSession=Valid", "");
   window.location.href = "https://aarav18-snhs.netlify.app/login.html";
 }
