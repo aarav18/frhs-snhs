6fae5b7937f959c32446b75835e6b1893cefa6c8
aarav18 <aaravshah18@gmail.com>
2022-04-25 21:22:16 -0600

    working google login

--- site/js/login.js
+++ site/js/login.js
@@ -5,56 +5,41 @@ document.documentElement.classList.remove("no-js");
 //   location.href = "/login";
 // }
 
-// var auth2;
+function decodeJwtResponse(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
 
-// gapi.load("auth2", function() {
-//   auth2 = gapi.auth2.init({
-//     client_id: "400237668286-4l755cng626bfg4pp3qcf3d2hh0ffg36.apps.googleusercontent.com"
-//   });
-//   auth2.attachClickHandler("signin-button", {}, onSuccess, onFailure);
+  return JSON.parse(jsonPayload);
+};
 
-//   auth2.isSignedIn.listen(signinChanged);
-//   auth2.currentUser.listen(userChanged); // This is what you use to listen for user changes
-// });  
+function handleCredentialResponse(response) {
+  console.log("Encoded JWT ID token: " + response.credential);
+  const responsePayload = decodeJwtResponse(response.credential);
 
-// var signinChanged = function (val) {
-//     console.log("Signin state changed to ", val);
-// };
+  console.log("ID: " + responsePayload.sub);
+  console.log('Full Name: ' + responsePayload.name);
+  console.log('Given Name: ' + responsePayload.given_name);
+  console.log('Family Name: ' + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
 
-// var onSuccess = function(user) {
-//     console.log("Signed in as " + user.getBasicProfile().getName());
+  // redirect
+  window.location.href = "https://aarav18-snhs.netlify.app/home.html";
+}
 
-//     // redirect to login page if not signed in
-//     var sessionTimeout = 1; //hours
-//     var loginDuration = new Date();
-//     loginDuration.setTime(loginDuration.getTime()+(sessionTimeout*60*60*1000));
-//     // document.cookie = "CrewCentreSession=Valid; "+loginDuration.toGMTString()+"; path=/site/";
-//     // document.cookie = "CrewCentreSession=Valid; "+loginDuration.toGMTString()+"; path=/";
-    
-//     if (user.getBasicProfile().getName() == "FRHS SNHS") {
-//       location.replace(location.href.split("/login.html")[0] + "/home.html");
-//       // location.replace(location.href.split("/login")[0] + "/home");
-//     } else {
-//       console.log("Wrong Google Address for Sign In");
-//     }
-// };
-
-// var onFailure = function(error) {
-//   console.log(error);
-// };
-
-// function signOut() {
-//   console.log("Start Sign Out");
-//   auth2.signOut().then(function () {
-//       console.log("User signed out.");
-//   });
-// }        
-
-// var userChanged = function (user) {
-//     if(user.getId()){
-//       // Do something here
-//     }
-// };
-
-// document.getElementsByClassName("sign-out").onclick = function() {signOut()}
-// document.getElementsByClassName("sign-out").addEventListener("click", signOut);
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: "400237668286-4l755cng626bfg4pp3qcf3d2hh0ffg36.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("google-signin"),
+    { theme: "outline", 
+      size: "large"
+    }  // customization attributes
+  );
+  // google.accounts.id.prompt(); // also display the One Tap dialog
+}
