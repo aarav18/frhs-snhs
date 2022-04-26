f5def492fcf7143506d3063bafc07eb158bc26cc
aarav18 <aaravshah18@gmail.com>
2022-04-25 18:51:14 -0600

    comment login.js

--- site/js/login.js
+++ site/js/login.js
@@ -5,56 +5,56 @@ document.documentElement.classList.remove("no-js");
 //   location.href = "/login";
 // }
 
-var auth2;
+// var auth2;
 
-gapi.load("auth2", function() {
-  auth2 = gapi.auth2.init({
-    client_id: "400237668286-4l755cng626bfg4pp3qcf3d2hh0ffg36.apps.googleusercontent.com"
-  });
-  auth2.attachClickHandler("signin-button", {}, onSuccess, onFailure);
+// gapi.load("auth2", function() {
+//   auth2 = gapi.auth2.init({
+//     client_id: "400237668286-4l755cng626bfg4pp3qcf3d2hh0ffg36.apps.googleusercontent.com"
+//   });
+//   auth2.attachClickHandler("signin-button", {}, onSuccess, onFailure);
 
-  auth2.isSignedIn.listen(signinChanged);
-  auth2.currentUser.listen(userChanged); // This is what you use to listen for user changes
-});  
+//   auth2.isSignedIn.listen(signinChanged);
+//   auth2.currentUser.listen(userChanged); // This is what you use to listen for user changes
+// });  
 
-var signinChanged = function (val) {
-    console.log("Signin state changed to ", val);
-};
+// var signinChanged = function (val) {
+//     console.log("Signin state changed to ", val);
+// };
 
-var onSuccess = function(user) {
-    console.log("Signed in as " + user.getBasicProfile().getName());
+// var onSuccess = function(user) {
+//     console.log("Signed in as " + user.getBasicProfile().getName());
 
-    // redirect to login page if not signed in
-    var sessionTimeout = 1; //hours
-    var loginDuration = new Date();
-    loginDuration.setTime(loginDuration.getTime()+(sessionTimeout*60*60*1000));
-    // document.cookie = "CrewCentreSession=Valid; "+loginDuration.toGMTString()+"; path=/site/";
-    // document.cookie = "CrewCentreSession=Valid; "+loginDuration.toGMTString()+"; path=/";
+//     // redirect to login page if not signed in
+//     var sessionTimeout = 1; //hours
+//     var loginDuration = new Date();
+//     loginDuration.setTime(loginDuration.getTime()+(sessionTimeout*60*60*1000));
+//     // document.cookie = "CrewCentreSession=Valid; "+loginDuration.toGMTString()+"; path=/site/";
+//     // document.cookie = "CrewCentreSession=Valid; "+loginDuration.toGMTString()+"; path=/";
     
-    if (user.getBasicProfile().getName() == "FRHS SNHS") {
-      location.replace(location.href.split("/login.html")[0] + "/home.html");
-      // location.replace(location.href.split("/login")[0] + "/home");
-    } else {
-      console.log("Wrong Google Address for Sign In");
-    }
-};
+//     if (user.getBasicProfile().getName() == "FRHS SNHS") {
+//       location.replace(location.href.split("/login.html")[0] + "/home.html");
+//       // location.replace(location.href.split("/login")[0] + "/home");
+//     } else {
+//       console.log("Wrong Google Address for Sign In");
+//     }
+// };
 
-var onFailure = function(error) {
-  console.log(error);
-};
+// var onFailure = function(error) {
+//   console.log(error);
+// };
 
-function signOut() {
-  console.log("Start Sign Out");
-  auth2.signOut().then(function () {
-      console.log("User signed out.");
-  });
-}        
+// function signOut() {
+//   console.log("Start Sign Out");
+//   auth2.signOut().then(function () {
+//       console.log("User signed out.");
+//   });
+// }        
 
-var userChanged = function (user) {
-    if(user.getId()){
-      // Do something here
-    }
-};
+// var userChanged = function (user) {
+//     if(user.getId()){
+//       // Do something here
+//     }
+// };
 
 // document.getElementsByClassName("sign-out").onclick = function() {signOut()}
 // document.getElementsByClassName("sign-out").addEventListener("click", signOut);
